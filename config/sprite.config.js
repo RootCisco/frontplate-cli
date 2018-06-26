a9097affdaaca5e84b1ce999fb194b0dfc5542f6
Cisco.K <exiria7@gmail.com>
2018-06-26 17:40:23 +0900

    refactor: spriteの出力先を変更。

--- config/sprite.config.js
+++ config/sprite.config.js
@@ -3,7 +3,7 @@ module.exports = [
     {
         src: `${FRP_SRC}/sprites/icon/*.{png,gif,jpg}`,    // 読み込む画像
         destImage: `${FRP_SRC}/images/icon.png`,           // 画像出力先
-        destCSS: `${FRP_SRC}/sass/sprites/_icon.scss`,     // CSS出力先
+        destCSS: `${FRP_SRC}/style/sprites/_icon.scss`,     // CSS出力先
         imgPath: '../images/icon.png',              // CSS内での画像パス
         padding: 2,                                 // 画像の間隔
         scale: 1.0,                                 // 画像表示サイズ（Retina対応なら0.5にする）
